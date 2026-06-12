e4ca11704239dda7a306985d379599951a536ec2
PixelHaven <holywuya@outlook.com>
2026-06-12 11:05:45 +0800

    feat: upgrade to Luminol BufferedLinearRegionFile + unsafe teleportation
    - Replace old LinearRegionFile with Luminol's BufferedLinearRegionFile
    - Add BufferedLinearRegionFileFlusher for async flushing
    - Add abomination/LinearRegionFile as old format reader
    - Add entities.allowUnsafeTeleportation config from Luminol
    - Update EnumRegionFormat to use new implementation

--- canvas-server/src/main/java/io/canvasmc/canvas/region/EnumRegionFormat.java
+++ canvas-server/src/main/java/io/canvasmc/canvas/region/EnumRegionFormat.java
@@ -1,6 +1,7 @@
 package io.canvasmc.canvas.region;
 
-import io.canvasmc.canvas.region.linear.LinearRegionFile;
+import io.canvasmc.canvas.region.linear.BufferedLinearRegionFile;
+import io.canvasmc.canvas.region.linear.BufferedLinearRegionFileFlusher;
 import net.minecraft.world.level.chunk.storage.RegionFile;
 import net.minecraft.world.level.chunk.storage.RegionStorageInfo;
 import org.jetbrains.annotations.Nullable;
@@ -12,7 +13,10 @@ public enum EnumRegionFormat {
     MCA("mca", "mca",
         (info) -> new RegionFile(info.info(), info.filePath(), info.folder(), info.sync())),
     LINEAR_V2("linear_v2", "linear",
-        (info) -> new LinearRegionFile(info.info(), info.filePath(), info.folder(), info.sync(), 1));
+        (info) -> {
+            final BufferedLinearRegionFileFlusher flusher = BufferedLinearRegionFileFlusher.getOrCreate(info.folder());
+            return new BufferedLinearRegionFile(info.filePath(), 1, flusher);
+        });
 
     private final String name;
     private final String argument;
